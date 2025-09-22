9e7acb1b4bced8c2d856b2b152ff14112d5c11da
navya <navyapkhajuria@gmail.com>
2025-09-22 14:55:59 +0530

    Create experiment_16(a).cpp

---
+++ experiment_16(a).cpp
@@ -0,0 +1,28 @@
+//Navya Pawan Khajuria
+//24070123151
+//B3
+
+#include <iostream>
+int main() {
+  float n1, n2, ans;
+  cout<<"Enter values of numbers 1 & 2: ";
+  cin>>n1>>n2;
+  try{
+      if(n2==0){
+          throw n2;
+      }
+      else{
+          ans=n1/n2;
+          cout<<"Answer = "<<ans<<endl;
+      }
+  }
+  catch(float num){
+      cout<<"\nERROR: Division by "<<num<<endl;
+  }
+}
+
+//Output:
+//Enter values of numbers 1 & 2:
+//6
+//3
+//Answer = 2
